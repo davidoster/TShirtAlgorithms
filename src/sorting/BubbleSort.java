5fac7ff0e6c0507e99541956a1f080822a58d4fb
David Oster <paspa@hotmail.com>
2020-07-10 20:34:26 +0300

    - BubbleSort fully implemented for TShirt - Sort by order ASC, DESC - Fully tested - OK!

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -5,36 +5,178 @@
  */
 package sorting;
 
+import java.util.List;
+import tshirtalgorithms.models.TShirt;
+
 /**
  *
  * @author mac
  */
 // https://www.geeksforgeeks.org/bubble-sort/
 public class BubbleSort {
-    static void bubbleSort(int arr[], int n) 
+    // type = 0, SIZE
+    // type = 1, COLOR
+    // type = 2, FABRIC
+    
+    // order = 0, ASC
+    // order = 1, DESC
+    public List<TShirt> sort(List<TShirt> arr, int n, byte order, byte type) 
     { 
         int i, j, temp; 
         boolean swapped; 
-        for (i = 0; i < n - 1; i++)  
-        { 
-            swapped = false; 
-            for (j = 0; j < n - i - 1; j++)  
-            { 
-                if (arr[j] > arr[j + 1])  
-                { 
-                    // swap arr[j] and arr[j+1] 
-                    temp = arr[j]; 
-                    arr[j] = arr[j + 1]; 
-                    arr[j + 1] = temp; 
-                    swapped = true; 
-                } 
-            } 
-  
-            // IF no two elements were  
-            // swapped by inner loop, then break 
-            if (swapped == false) 
-                break; 
-        } 
+        switch(order) {
+            // ASC
+            case 0:
+                switch(type) {
+                    // SIZE
+                    case 0:
+                        for (i = 0; i < n - 1; i++)  
+                        { 
+                            swapped = false; 
+                            for (j = 0; j < n - i - 1; j++)  
+                            { 
+                                if (arr.get(j).getS().ordinal() > arr.get(j + 1).getS().ordinal())  
+                                { 
+                                    // swap arr[j] and arr[j+1] 
+                                    TShirt temp2 = arr.get(j); 
+                                    arr.set(j, arr.get(j + 1));
+                                    arr.set(j + 1, temp2); 
+                                    swapped = true; 
+                                } 
+                            } 
+
+                            // IF no two elements were  
+                            // swapped by inner loop, then break 
+                            if (swapped == false) 
+                                break; 
+                        }
+                        break;
+                    // COLOR    
+                    case 1:
+                        for (i = 0; i < n - 1; i++)  
+                        { 
+                            swapped = false; 
+                            for (j = 0; j < n - i - 1; j++)  
+                            { 
+                                if (arr.get(j).getC().ordinal() > arr.get(j + 1).getC().ordinal())  
+                                { 
+                                    // swap arr[j] and arr[j+1] 
+                                    TShirt temp2 = arr.get(j); 
+                                    arr.set(j, arr.get(j + 1));
+                                    arr.set(j + 1, temp2); 
+                                    swapped = true; 
+                                } 
+                            } 
+
+                            // IF no two elements were  
+                            // swapped by inner loop, then break 
+                            if (swapped == false) 
+                                break; 
+                        }
+                        break;
+                    // FABRIC
+                    case 2:
+                        for (i = 0; i < n - 1; i++)  
+                        { 
+                            swapped = false; 
+                            for (j = 0; j < n - i - 1; j++)  
+                            { 
+                                if (arr.get(j).getF().ordinal() > arr.get(j + 1).getF().ordinal())  
+                                { 
+                                    // swap arr[j] and arr[j+1] 
+                                    TShirt temp2 = arr.get(j); 
+                                    arr.set(j, arr.get(j + 1));
+                                    arr.set(j + 1, temp2); 
+                                    swapped = true; 
+                                } 
+                            } 
+
+                            // IF no two elements were  
+                            // swapped by inner loop, then break 
+                            if (swapped == false) 
+                                break; 
+                        }
+                        break;
+                }
+                
+                break;
+            // DESC
+            case 1:
+                switch(type) {
+                    // SIZE
+                    case 0:
+                        for (i = 0; i < n - 1; i++)  
+                        { 
+                            swapped = false; 
+                            for (j = 0; j < n - i - 1; j++)  
+                            { 
+                                if (arr.get(j).getS().ordinal() < arr.get(j + 1).getS().ordinal())  
+                                { 
+                                    // swap arr[j] and arr[j+1] 
+                                    TShirt temp2 = arr.get(j); 
+                                    arr.set(j, arr.get(j + 1));
+                                    arr.set(j + 1, temp2); 
+                                    swapped = true; 
+                                } 
+                            } 
+
+                            // IF no two elements were  
+                            // swapped by inner loop, then break 
+                            if (swapped == false) 
+                                break; 
+                        }
+                        break;
+                    // COLOR    
+                    case 1:
+                        for (i = 0; i < n - 1; i++)  
+                        { 
+                            swapped = false; 
+                            for (j = 0; j < n - i - 1; j++)  
+                            { 
+                                if (arr.get(j).getC().ordinal() < arr.get(j + 1).getC().ordinal())  
+                                { 
+                                    // swap arr[j] and arr[j+1] 
+                                    TShirt temp2 = arr.get(j); 
+                                    arr.set(j, arr.get(j + 1));
+                                    arr.set(j + 1, temp2); 
+                                    swapped = true; 
+                                } 
+                            } 
+
+                            // IF no two elements were  
+                            // swapped by inner loop, then break 
+                            if (swapped == false) 
+                                break; 
+                        }
+                        break;
+                    // FABRIC
+                    case 2:
+                        for (i = 0; i < n - 1; i++)  
+                        { 
+                            swapped = false; 
+                            for (j = 0; j < n - i - 1; j++)  
+                            { 
+                                if (arr.get(j).getF().ordinal() < arr.get(j + 1).getF().ordinal())  
+                                { 
+                                    // swap arr[j] and arr[j+1] 
+                                    TShirt temp2 = arr.get(j); 
+                                    arr.set(j, arr.get(j + 1));
+                                    arr.set(j + 1, temp2); 
+                                    swapped = true; 
+                                } 
+                            } 
+
+                            // IF no two elements were  
+                            // swapped by inner loop, then break 
+                            if (swapped == false) 
+                                break; 
+                        }
+                        break;
+                }
+                break;
+                
+        }
+        return arr;
     } 
   
     // Function to print an array  
--- src/tshirtalgorithms/TShirtAlgorithms.java
+++ src/tshirtalgorithms/TShirtAlgorithms.java
@@ -8,6 +8,7 @@ package tshirtalgorithms;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import sorting.BubbleSort;
 import sorting.QuickSort;
 import tshirtalgorithms.models.Color;
 import tshirtalgorithms.models.Fabric;
@@ -24,44 +25,58 @@ public class TShirtAlgorithms {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // order = 0, ASC
+        // order = 1, DESC
+
         // type = 0, SIZE
         // type = 1, COLOR
         // type = 2, FABRIC
-        
-        
         int high = 6;
         List<TShirt> tShirts = generateTShirts(high + 1);
         System.out.println("Unsorted array");
         System.out.println("--------------");
         printTShirts(tShirts);
         
-//        QuickSort qs = new QuickSort();
-//        List<TShirt> qsSortedBySizeASC = new ArrayList<TShirt>(tShirts);
-//        qsSortedBySizeASC = qs.sort(qsSortedBySizeASC, 0, high, (byte)0, (byte)0);
-//        System.out.println("\nSorted array By Size ASC");
-//        System.out.println("--------------");
-//        printTShirts(qsSortedBySizeASC);
+//        doQuickSort(tShirts,high);
+        doBubbleSort(tShirts);
+        
+        
+        
+        
+        
+        
 //        
-//        List<TShirt> qsSortedBySizeDESC = new ArrayList<TShirt>(tShirts);
-//        qsSortedBySizeASC = qs.sort(qsSortedBySizeDESC, 0, high, (byte)1, (byte)0);
-//        System.out.println("\nSorted array By Size DESC");
-//        System.out.println("--------------");
-//        printTShirts(qsSortedBySizeDESC);
-
-//        QuickSort qs = new QuickSort();
-//        List<TShirt> qsSortedByColorASC = new ArrayList<TShirt>(tShirts);
-//        qsSortedByColorASC = qs.sort(qsSortedByColorASC, 0, high, (byte)0, (byte)1);
-//        System.out.println("\nSorted array By Color ASC");
-//        System.out.println("--------------");
-//        printTShirts(qsSortedByColorASC);
-//        
-//        List<TShirt> qsSortedByColorDESC = new ArrayList<TShirt>(tShirts);
-//        qsSortedByColorDESC = qs.sort(qsSortedByColorDESC, 0, high, (byte)1, (byte)1);
-//        System.out.println("\nSorted array By Color DESC");
-//        System.out.println("--------------");
-//        printTShirts(qsSortedByColorDESC);
-
+        
+        
+        
+    }
+    
+    public static void doQuickSort(List<TShirt> tShirts, int high) {
         QuickSort qs = new QuickSort();
+        List<TShirt> qsSortedBySizeASC = new ArrayList<TShirt>(tShirts);
+        qsSortedBySizeASC = qs.sort(qsSortedBySizeASC, 0, high, (byte)0, (byte)0);
+        System.out.println("\nSorted array By Size ASC");
+        System.out.println("--------------");
+        printTShirts(qsSortedBySizeASC);
+        
+        List<TShirt> qsSortedBySizeDESC = new ArrayList<TShirt>(tShirts);
+        qsSortedBySizeASC = qs.sort(qsSortedBySizeDESC, 0, high, (byte)1, (byte)0);
+        System.out.println("\nSorted array By Size DESC");
+        System.out.println("--------------");
+        printTShirts(qsSortedBySizeDESC);
+      
+        List<TShirt> qsSortedByColorASC = new ArrayList<TShirt>(tShirts);
+        qsSortedByColorASC = qs.sort(qsSortedByColorASC, 0, high, (byte)0, (byte)1);
+        System.out.println("\nSorted array By Color ASC");
+        System.out.println("--------------");
+        printTShirts(qsSortedByColorASC);
+        
+        List<TShirt> qsSortedByColorDESC = new ArrayList<TShirt>(tShirts);
+        qsSortedByColorDESC = qs.sort(qsSortedByColorDESC, 0, high, (byte)1, (byte)1);
+        System.out.println("\nSorted array By Color DESC");
+        System.out.println("--------------");
+        printTShirts(qsSortedByColorDESC);
+
         List<TShirt> qsSortedByFabricASC = new ArrayList<TShirt>(tShirts);
         qsSortedByFabricASC = qs.sort(qsSortedByFabricASC, 0, high, (byte)0, (byte)2);
         System.out.println("\nSorted array By Fabric ASC");
@@ -73,6 +88,46 @@ public class TShirtAlgorithms {
         System.out.println("\nSorted array By Fabric DESC");
         System.out.println("--------------");
         printTShirts(qsSortedByFabricDESC);
+        
+    }
+    
+    public static void doBubbleSort(List<TShirt> tShirts) {
+        BubbleSort bs  = new BubbleSort();
+        List<TShirt> bsSortedBySizeASC = new ArrayList<TShirt>(tShirts);
+        bsSortedBySizeASC = bs.sort(bsSortedBySizeASC, tShirts.size(), (byte)0, (byte)0);
+        System.out.println("\nSorted array By Size ASC");
+        System.out.println("--------------");
+        printTShirts(bsSortedBySizeASC);
+        
+        List<TShirt> bsSortedBySizeDESC = new ArrayList<TShirt>(tShirts);
+        bsSortedBySizeDESC = bs.sort(bsSortedBySizeDESC, tShirts.size(), (byte)1, (byte)0);
+        System.out.println("\nSorted array By Size DESC");
+        System.out.println("--------------");
+        printTShirts(bsSortedBySizeDESC);
+        
+        List<TShirt> bsSortedByColorASC = new ArrayList<TShirt>(tShirts);
+        bsSortedByColorASC = bs.sort(bsSortedByColorASC, tShirts.size(), (byte)0, (byte)1);
+        System.out.println("\nSorted array By Color ASC");
+        System.out.println("--------------");
+        printTShirts(bsSortedByColorASC);
+        
+        List<TShirt> bsSortedByColorDESC = new ArrayList<TShirt>(tShirts);
+        bsSortedByColorDESC = bs.sort(bsSortedByColorDESC, tShirts.size(), (byte)1, (byte)1);
+        System.out.println("\nSorted array By Color DESC");
+        System.out.println("--------------");
+        printTShirts(bsSortedByColorDESC);
+        
+        List<TShirt> bsSortedByFabricASC = new ArrayList<TShirt>(tShirts);
+        bsSortedByFabricASC = bs.sort(bsSortedByFabricASC, tShirts.size(), (byte)0, (byte)2);
+        System.out.println("\nSorted array By Fabric ASC");
+        System.out.println("--------------");
+        printTShirts(bsSortedByFabricASC);
+        
+        List<TShirt> bsSortedByFabricDESC = new ArrayList<TShirt>(tShirts);
+        bsSortedByFabricDESC = bs.sort(bsSortedByFabricDESC, tShirts.size(), (byte)1, (byte)2);
+        System.out.println("\nSorted array By Fabric DESC");
+        System.out.println("--------------");
+        printTShirts(bsSortedByFabricDESC);
     }
     
     public static List<TShirt> generateTShirts(int count) {
